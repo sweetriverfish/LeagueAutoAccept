771478104edeec817989937363818fea154be23d
Artem <artemx100@gmail.com>
2023-09-01 21:13:05 +0300

    Add delay to chat messages
    Addressing issue #17, adding a 100ms delay after joining champ select before sending messages in chat to give time for others to join chat

--- Leauge Auto Accept/MainLogic.cs
+++ Leauge Auto Accept/MainLogic.cs
@@ -184,25 +184,36 @@ namespace Leauge_Auto_Accept
         private static void handleChampSelectChat(string chatId)
         {
             string[] chats = LCU.clientRequest("GET", "lol-chat/v1/conversations", "");
-            if (chats[1].Contains(chatId)) {
+            if (chats[1].Contains(chatId))
+            {
                 Data.loadPlayerChatId();
 
-                string timestamp = DateTime.UtcNow.ToString("yyyy-MM-ddTHH:mm:ssZ");
-                foreach (var message in Settings.chatMessages)
-                {
-                    int attempts = 0;
-                    string httpRes = "";
-                    while (httpRes != "200" && attempts < 3)
-                    {
-                        string body = "{\"type\":\"chat\",\"fromId\":\"" + Data.currentChatId + "\",\"fromSummonerId\":" + Data.currentSummonerId + ",\"isHistorical\":false,\"timestamp\":\"" + timestamp + "\",\"body\":\"" + message + "\"}";
-                        string[] response = LCU.clientRequest("POST", "lol-chat/v1/conversations/" + chatId + "/messages", body);
-                        attempts++;
-                        httpRes = response[0];
-                        Thread.Sleep(attempts * 20);
-                    }
+                long currentTime = DateTimeOffset.Now.ToUnixTimeMilliseconds();
+
+                if ((currentTime - 100) > champSelectStart) {
+                    handleChampSelectChatSendMsg(chatId);
                 }
-                sentChatMessages = true;
+
             }
+        }
+
+        private static void handleChampSelectChatSendMsg(string chatId)
+        {
+            foreach (var message in Settings.chatMessages)
+            {
+                int attempts = 0;
+                string httpRes = "";
+                while (httpRes != "200" && attempts < 3)
+                {
+                    string timestamp = DateTime.UtcNow.ToString("yyyy-MM-ddTHH:mm:ssZ");
+                    string body = "{\"type\":\"chat\",\"fromId\":\"" + Data.currentChatId + "\",\"fromSummonerId\":" + Data.currentSummonerId + ",\"isHistorical\":false,\"timestamp\":\"" + timestamp + "\",\"body\":\"" + message + "\"}";
+                    string[] response = LCU.clientRequest("POST", "lol-chat/v1/conversations/" + chatId + "/messages", body);
+                    attempts++;
+                    httpRes = response[0];
+                    Thread.Sleep(attempts * 20);
+                }
+            }
+            sentChatMessages = true;
         }
 
         private static void handleChampSelectActions(string[] currentChampSelect, string localPlayerCellId)
@@ -346,7 +357,7 @@ namespace Leauge_Auto_Accept
                 lockChampion(actId, championId, actType);
             }
         }
-
+        
         private static void handlePickOrderSwap()
         {
             // Return if we already locked in or if the settings is off
